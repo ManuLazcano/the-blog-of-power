6f4fcb2851df2b7e58b9a4f6cac3ed5f08aad718
ManuLazcano <manu.lazcano17@gmail.com>
2024-10-30 12:41:28 -0300

    feat(backend): create zod schema for user

---
+++ backend/schemas/users.js
@@ -0,0 +1,27 @@
+import z from 'zod'
+
+const userSchema = z.object({
+  id: z.string().uuid('El ID de usuario debe ser un UUID válido'),
+  nick_name: z.string()
+    .min(1, 'El campo "ninck_name" no puede estar vacío')
+    .max(30, 'El campo "ninck_name" no debe exceder los 30 caracteres'),
+  name: z.string()
+    .min(1, 'El campo "name" no puede estar vacío')
+    .max(50, 'El campo "name" no debe exceder los 50 caracteres'),
+  email: z.string()
+    .email('El correo electrónico no es válido')
+    .min(1, 'El campo "email" no puede estar vacío')
+    .max(60, 'El "email" no debe exceder los 60 caracteres'),
+  password: z.string().min(1, 'El campo "password" no puede estar vacío'),
+  RolId: z.number()
+    .int()
+    .positive('El ID del rol debe ser un número entero positivo')
+})
+
+export const validateUser = (object) => {
+  return userSchema.safeParse(object)
+}
+
+export const validateParcialUser = (object) => {
+  return userSchema.partial().safeParse(object)
+}
