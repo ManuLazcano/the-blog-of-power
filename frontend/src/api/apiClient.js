e7451dd9e7369bdbfaf8c33b33798ae02f42b40b
ManuLazcano <manu.lazcano17@gmail.com>
2024-11-24 10:08:59 -0300

    feat(frontend): configure axios

---
+++ frontend/src/api/apiClient.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('Error en la solicitud: ', error.response?.data?.message || error.message)
+    
+  }
+)
+
+export { apiClient }
