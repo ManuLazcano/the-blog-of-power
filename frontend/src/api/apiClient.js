cb5270ad19f0419762efcdbd6485c6a8ea779146
ManuLazcano <manu.lazcano17@gmail.com>
2024-11-28 17:28:30 -0300

    feat(frontend): Allow sending cookies for authentication

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -5,14 +5,14 @@ const apiClient = axios.create({
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
-  }
+  },
+  withCredentials: true
 })
 
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    console.error('Error en la solicitud: ', error.response?.data?.message || error.message)
-    
+    console.error('Error en la solicitud: ', error.response?.data?.message || error.message)    
   }
 )
 
