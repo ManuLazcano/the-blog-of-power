babd36a8ac30a8c5b15189e454a48a70ee6cee28
ManuLazcano <manu.lazcano17@gmail.com>
2024-11-28 09:43:50 -0300

    feat(frontend): create login form

--- frontend/src/pages/SingIn/index.jsx
+++ frontend/src/pages/SingIn/index.jsx
@@ -1,6 +1,75 @@
+import { useState } from "react";
+
 const SingIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log('Enviado');    
+  };
+
   return (
-    <h1>SingIn</h1>
+    <div className="w-full flex items-center justify-center h-screen bg-gray-100">
+      <form 
+        onSubmit={handleSubmit} 
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 w-full max-w-sm"
+      >
+        <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
+        
+        {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
+        
+        <div className="mb-4">
+          <label 
+            htmlFor="email" 
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Ingresa tu email"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label 
+            htmlFor="password" 
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Contraseña:
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Ingresa tu contraseña"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        <div className="mt-6 flex flex-col gap-4">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Iniciar sesión
+          </button>
+          <a // TODO: Link
+            href="/register" 
+            className="inline-block align-baseline font-bold text-md text-gray-500 hover:text-gray-800 self-center"
+          >
+            Crear cuenta
+          </a>
+        </div>
+      </form>
+    </div>
   )
 }
 
