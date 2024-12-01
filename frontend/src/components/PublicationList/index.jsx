f86f6ed8a0938fc7212f0d8e69801fc58f84ddc2
ManuLazcano <manu.lazcano17@gmail.com>
2024-11-30 23:16:26 -0300

    feat(frontend): adjust maximun width of item product

--- frontend/src/components/PublicationList/index.jsx
+++ frontend/src/components/PublicationList/index.jsx
@@ -1,6 +1,6 @@
 const PublicationList = ({ children }) => {
   return(
-    <section className="grid grid-cols-1 sm:grid-cols-2 gap-6 p-4">
+    <section className="grid grid-cols-1 sm:grid-cols-2 gap-6 p-4 w-full max-w-6xl">
       {children}
     </section>     
   )
