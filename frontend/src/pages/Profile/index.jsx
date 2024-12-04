bbf8014999d52ecd2eda06988f0eef42fb5d2602
ManuLazcano <manu.lazcano17@gmail.com>
2024-12-04 11:49:11 -0300

    feat(frontend): create profile design

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,9 +1,42 @@
+import { useUser } from '../../hooks/useUser'
+
 const Perfil = () => {
+  const { user, loading, error } = useUser()
 
   return (
-    <section>
-      <article>    
-      </article>
+    <section className="h-screen w-full p-6 flex items-center justify-center">
+      {loading && <p>Cargando...</p> /**TODO: Crear su propio componente */}
+      {error && <p>Hubo un error</p> /**TODO: Crear su propio componente */}
+      {!loading && user &&
+        <article className="bg-white shadow-md rounded-lg w-full max-w-sm p-6 mx-auto">
+          <header className="mb-6 text-center">
+            <h1 className="text-2xl font-bold text-gray-800">Perfil de Usuario</h1>
+            <p className="text-gray-500">Información personal</p>
+          </header>
+          <div className="space-y-4">
+            <div>
+              <label className="block text-gray-600 font-medium">Nickname</label>
+              <p className="bg-gray-100 border border-gray-300 rounded px-4 py-2">{user.nick_name}</p>
+            </div>
+            <div>
+              <label className="block text-gray-600 font-medium">Nombre</label>
+              <p className="bg-gray-100 border border-gray-300 rounded px-4 py-2">{user.name}</p>
+            </div>
+            <div>
+              <label className="block text-gray-600 font-medium">Email</label>
+              <p className="bg-gray-100 border border-gray-300 rounded px-4 py-2">{user.email}</p>
+            </div>
+          </div>
+          <footer className="mt-6 text-center">
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+               onClick={() => console.log('Editar perfil')}
+            >
+              Editar Perfil
+            </button>
+          </footer>
+        </article>
+      }
     </section>
   )
 }
