538bf417b895df6999a87b4d706067593c827133
ManuLazcano <manu.lazcano17@gmail.com>
2024-12-06 12:39:03 -0300

    style(frontend): modify tabs and delete semicolons

--- frontend/src/components/loadings-skeleton/EditProfileSkeleton.jsx
+++ frontend/src/components/loadings-skeleton/EditProfileSkeleton.jsx
@@ -1,34 +1,34 @@
 const EditProfileSkeleton = () => {
-    return (
-      <div className="w-full max-w-sm flex items-center justify-center bg-gray-100 p-6 mt-8">
-        <div className="bg-white shadow-md rounded-lg w-full max-w-md p-6 space-y-4 animate-pulse">
-          <header className="mb-4 text-center">
-            <div className="h-6 bg-gray-300 rounded w-1/2 mx-auto"></div>
-            <div className="h-4 bg-gray-300 rounded w-1/3 mx-auto mt-2"></div>
-          </header>
-          <div className="space-y-3">
-            <div>
-              <div className="h-4 bg-gray-300 rounded w-1/4 mb-2"></div>
-              <div className="h-10 bg-gray-300 rounded w-full"></div>
-            </div>
-            <div>
-              <div className="h-4 bg-gray-300 rounded w-1/4 mb-2"></div>
-              <div className="h-10 bg-gray-300 rounded w-full"></div>
-            </div>
-            <div>
-              <div className="h-4 bg-gray-300 rounded w-1/4 mb-2"></div>
-              <div className="h-10 bg-gray-300 rounded w-full"></div>
-            </div>
+  return (
+    <div className="w-full max-w-sm flex items-center justify-center bg-gray-100 p-6 mt-8">
+      <div className="bg-white shadow-md rounded-lg w-full max-w-md p-6 space-y-4 animate-pulse">
+        <header className="mb-4 text-center">
+          <div className="h-6 bg-gray-300 rounded w-1/2 mx-auto"></div>
+          <div className="h-4 bg-gray-300 rounded w-1/3 mx-auto mt-2"></div>
+        </header>
+        <div className="space-y-3">
+          <div>
+            <div className="h-4 bg-gray-300 rounded w-1/4 mb-2"></div>
+            <div className="h-10 bg-gray-300 rounded w-full"></div>
           </div>
-          <div className="flex justify-between mt-6">
-            <div className="h-10 bg-gray-300 rounded w-24"></div>
-            <div className="h-10 bg-gray-300 rounded w-24"></div>
+          <div>
+            <div className="h-4 bg-gray-300 rounded w-1/4 mb-2"></div>
+            <div className="h-10 bg-gray-300 rounded w-full"></div>
           </div>
-          <div className="h-4 bg-gray-300 rounded w-1/3 mx-auto mt-6"></div>
+          <div>
+            <div className="h-4 bg-gray-300 rounded w-1/4 mb-2"></div>
+            <div className="h-10 bg-gray-300 rounded w-full"></div>
+          </div>
         </div>
+        <div className="flex justify-between mt-6">
+          <div className="h-10 bg-gray-300 rounded w-24"></div>
+          <div className="h-10 bg-gray-300 rounded w-24"></div>
+        </div>
+        <div className="h-4 bg-gray-300 rounded w-1/3 mx-auto mt-6"></div>
       </div>
-    );
-  };
+    </div>
+  )
+}
   
-  export { EditProfileSkeleton };
+export { EditProfileSkeleton }
   
--- frontend/src/components/loadings-skeleton/PublicationDetailSkeleton.jsx
+++ frontend/src/components/loadings-skeleton/PublicationDetailSkeleton.jsx
@@ -1,25 +1,25 @@
 const PublicationDetailSkeleton = () => {
-    return (
-      <article className="bg-gray-200 shadow-md rounded-lg w-full p-6 max-w-2xl mx-auto h-auto min-h-[500px] sm:min-h-[600px] flex flex-col animate-pulse">
-        <header>
-          <div className="h-8 bg-gray-300 rounded mb-4 w-3/4"></div>
-        </header>
-        <div className="flex-grow space-y-3">
-          <div className="h-4 bg-gray-300 rounded w-full"></div>
-          <div className="h-4 bg-gray-300 rounded w-5/6"></div>
-          <div className="h-4 bg-gray-300 rounded w-4/6"></div>
-          <div className="h-4 bg-gray-300 rounded w-full"></div>
+  return (
+    <article className="bg-gray-200 shadow-md rounded-lg w-full p-6 max-w-2xl mx-auto h-auto min-h-[500px] sm:min-h-[600px] flex flex-col animate-pulse">
+      <header>
+        <div className="h-8 bg-gray-300 rounded mb-4 w-3/4"></div>
+      </header>
+      <div className="flex-grow space-y-3">
+        <div className="h-4 bg-gray-300 rounded w-full"></div>
+        <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+        <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+        <div className="h-4 bg-gray-300 rounded w-full"></div>
+      </div>
+      <footer className="mt-6 space-y-3">
+        <div className="h-8 bg-gray-300 rounded w-24"></div>
+        <div className="flex justify-between items-center">
+          <div className="h-4 bg-gray-300 rounded w-1/3"></div>
+          <div className="h-4 bg-gray-300 rounded w-1/4"></div>
         </div>
-        <footer className="mt-6 space-y-3">
-          <div className="h-8 bg-gray-300 rounded w-24"></div>
-          <div className="flex justify-between items-center">
-            <div className="h-4 bg-gray-300 rounded w-1/3"></div>
-            <div className="h-4 bg-gray-300 rounded w-1/4"></div>
-          </div>
-        </footer>
-      </article>
-    );
-  };
+      </footer>
+    </article>
+  )
+}
   
-  export { PublicationDetailSkeleton };
+export { PublicationDetailSkeleton }
   
